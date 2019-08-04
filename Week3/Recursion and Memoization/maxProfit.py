32a0de02cd4fed024f476e042d2fe64fbae81156
itatchi42 <jonathan@heali.ai>
2019-08-03 22:07:51 -0400

    adding some probs

--- Week3/Recursion and Memoization/maxProfit.py
+++ Week3/Recursion and Memoization/maxProfit.py
@@ -1,6 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # Approach 2: GREEDY- Find a small one? Save it!
+        # Took 55 minutes...
         smallest, diff = float('inf'), 0
         for val in prices:
             smallest = min(smallest, val)
